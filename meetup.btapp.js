1178ebbe77db99f3a510397cae2d4b53f374962d
Patrick Williams <pwmckenna@gmail.com>
2012-05-01 18:39:26 -0700

    first commit

---
+++ meetup.btapp.js
@@ -0,0 +1,22 @@
+(function() {
+	function assert(b, err) { if(!b) throw err; }
+
+	function get_magnet_link(hash) {
+		return 'magnet:?xt=urn:btih:' + hash + '&tr=udp://tracker.openbittorrent.com:80/announce';
+	}
+
+	BtappMeetup = Backbone.Model.extend({
+		initialize: function() {
+			this.btapp = new Btapp;
+			this.btapp.live('torrent *', function(peer) {
+				this.trigger('meetup:peer', peer);
+			}, this);
+			this.btapp.live('add', function(add) {
+				var info_hash = this.get('id');
+				this.trigger('meetup:meetup', info_hash);
+				add.torrent(get_magnet_link(info_hash));
+			}, this);
+			this.btapp.connect();
+		}
+	});	
+}).call(this);
